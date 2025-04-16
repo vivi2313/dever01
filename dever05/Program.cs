a36556f5bfa2d6d721d66659cddf3514202ec7ba
iguinho-ux <igormachadosr@gmail.com>
2025-04-16 08:18:13 -0300

    dever05

---
+++ dever05/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine()); 
+
+        
+        if (numero % 3 == 0)
+        {
+            Console.WriteLine("O número digitado é múltiplo de 3.");
+        }
+        else
+        {
+            Console.WriteLine("O número digitado não é múltiplo de 3.");
+        }
+    }
+}
