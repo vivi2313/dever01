af709d4f4cc06159fb17c797476a894f47c73c81
iguinho-ux <igormachadosr@gmail.com>
2025-04-16 08:21:44 -0300

    dever 07

---
+++ dever07/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Digite o primeiro número: ");
+        double numero1 = double.Parse(Console.ReadLine());  
+
+        
+        Console.Write("Digite o segundo número: ");
+        double numero2 = double.Parse(Console.ReadLine());  
+
+       
+        if (numero1 == numero2)
+        {
+            Console.WriteLine("Os dois números são iguais.");
+        }
+        else
+        {
+            Console.WriteLine("Os dois números são diferentes.");
+        }
+    }
+}
