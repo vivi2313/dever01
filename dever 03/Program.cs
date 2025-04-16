b4ea7883ff03c75f9cc283f75b092c5397be5fb0
iguinho-ux <igormachadosr@gmail.com>
2025-04-16 08:14:04 -0300

    dever04

--- dever 03/Program.cs
+++ dever 03/Program.cs
@@ -2,16 +2,13 @@
 {
     static void Main()
     {
-        // Solicita ao usuário para digitar sua idade
         Console.WriteLine("Digite sua idade:");
 
-        // Lê a entrada do usuário como string
         string entrada = Console.ReadLine();
 
-        // Ver se a entrada é um número válido
+       
         bool numero = true;
 
-        // Ver se a entrada é composta apenas por números
         foreach (char c in entrada)
         {
             if (!Char.IsDigit(c))
@@ -21,12 +18,10 @@
             }
         }
 
-       
         if (numero)
         {
             int idade = Convert.ToInt32(entrada);
 
-            // Ver se a pessoa pode votar (idade maior ou igual a 16 anos)
             if (idade >= 16)
             {
                 Console.WriteLine("Vc pode votar.");
---
+++ dever04/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Digite uma letra: ");
+        char letra = Console.ReadKey().KeyChar; 
+
+        Console.WriteLine(); 
+
+        
+        if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
+            letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U')
+        {
+            Console.WriteLine("A letra digitada é uma vogal.");
+        }
+        else
+        {
+            Console.WriteLine("A letra digitada não é uma vogal.");
+        }
+    }
+}
