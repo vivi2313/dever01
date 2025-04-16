239fbe79b7cf1a092a479d62cc57e99e290d22dd
iguinho-ux <igormachadosr@gmail.com>
2025-04-16 08:34:06 -0300

    dever10

---
+++ dever 10/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        
+        string senha = "2313";  
+
+        Console.Write("Digite a senha: ");
+        string senhaDigitada = Console.ReadLine();  
+
+        if (senhaDigitada == senha)
+        {
+            Console.WriteLine("Senha correta!");
+        }
+        else
+        {
+            Console.WriteLine("Senha incorreta!");
+        }
+    }
+}
