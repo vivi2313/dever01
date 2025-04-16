872f6b0020fb9b6c75587db8abeba6635bccd2d1
iguinho-ux <igormachadosr@gmail.com>
2025-04-16 08:19:51 -0300

    dever06

---
+++ dever06/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+      
+        Console.Write("Digite a nota do aluno: ");
+        double nota = double.Parse(Console.ReadLine());  
+
+        if (nota >= 6)
+        {
+            Console.WriteLine("O aluno foi aprovado!");
+        }
+        else
+        {
+            Console.WriteLine("O aluno foi reprovado!");
+        }
+    }
+}
